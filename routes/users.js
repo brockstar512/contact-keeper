5384bacb1e42251f84107cc7d0d91f924f454396
marshall brock <marshallbrock@gmail.com>
2020-05-22 18:26:23 -0500

    update

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const router = express.Router()
-
+const User = require('../models/User')
 // @route   post  api/users
 //@description  register a user
 //@access  public
-router.post('/', (req, res)=>{
-    res.send('Register a user')
+router.post('/', 
+[
+    check('name', 'please add name').not().isEmpty(),
+    check('email', 'Pleas include a valid email').isEmail(),
+    check('password',"Please enter a password with 6 or more characters").isLength({min:6})
+],
+(req, res)=>{
+    res.send(req.body)
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
 })
 
 
